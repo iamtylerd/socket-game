db1be9b7d4a4121a744023e174480ae94a188178
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-05 11:41:07 -0500

    move game logic to server

--- public/main.js
+++ public/main.js
@@ -1,86 +1,56 @@
-'use strict';
+'use strict'
 
 const socket = io()
 
-socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
-socket.on('disconnect', () => console.log("Disconnected"))
+const board = document.querySelector('.board')
+const status = document.querySelector('.status')
 
-// Save board state
-const boardState = [
-['','',''],
-['','',''],
-['','','']
-]
+let nextPlayer = 'X'
 
-const drawBoard = (boardState) => {
-	document.querySelector('.board').innerHTML = `
+const renderStatus = game => {
+  status.innerText = game.result
+    ? `${game.result} WON!`
+    : `${game.toMove}'s Turn`
+}
+
+const renderBoard = game => {
+  const b = game.board
+
+  board.innerHTML = `
     <table>
       <tr>
-        <td>${boardState[0][0]}</td>
-        <td>${boardState[0][1]}</td>
-        <td>${boardState[0][2]}</td>
+        <td>${b[0][0]}</td>
+        <td>${b[0][1]}</td>
+        <td>${b[0][2]}</td>
       </tr>
       <tr>
-        <td>${boardState[1][0]}</td>
-        <td>${boardState[1][1]}</td>
-        <td>${boardState[1][2]}</td>
+        <td>${b[1][0]}</td>
+        <td>${b[1][1]}</td>
+        <td>${b[1][2]}</td>
       </tr>
       <tr>
-        <td>${boardState[2][0]}</td>
-        <td>${boardState[2][1]}</td>
-        <td>${boardState[2][2]}</td>
+        <td>${b[2][0]}</td>
+        <td>${b[2][1]}</td>
+        <td>${b[2][2]}</td>
       </tr>
     </table>
   `
 }
 
-const winner = b => {
-  // Rows
-  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
-    return b[0][0]
-  } else if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
-    return b[1][0]
-  } else if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
-    return b[2][0]
-  }
-
-  // Cols
-  else if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
-    return b[0][0]
-  } else if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
-    return b[0][1]
-  } else if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
-    return b[0][2]
-  }
-
-  // Diags
-  else if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
-    return b[0][0]
-  } else if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
-    return b[0][2]
-  }
-
-  // Tie or In-Progress
-  else {
-    return null
-  }
-}
-
-drawBoard(boardState)
-let nextPlayer = 'X'
-
-const board = document.querySelector('.board')
 board.addEventListener('click', evt => {
-	const col = evt.target.cellIndex
-	const row = evt.target.parentElement.rowIndex
+  const col = evt.target.cellIndex
+  const row = evt.target.closest('tr').rowIndex
 
-	if (boardState[row][col]) {
-		return console.log('You cant move here')
-	}
-		boardState[row][col] = nextPlayer
-		nextPlayer = nextPlayer === 'X' ? 'O' : 'X'
-		drawBoard(boardState)
-
-	console.log("You clicked on:", row, col)
+  socket.emit('make move', { row, col })
 })
 
+const render = game => {
+  renderStatus(game)
+  renderBoard(game)
+}
+
+socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
+socket.on('disconnect', () => console.log('Socket disconnected'))
+socket.on('error', console.error)
+socket.on('new game', render)
+socket.on('move made', render)
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const express = require('express')
 const { Server } = require('http')
@@ -10,22 +10,122 @@ const server = Server(app)
 const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
-const MONGOD_URL = process.env.MONGOD_URL || 'mongodb://localhost:27017/tickietackietoe'
-//set up pug
+const MONGODB_URL =  process.env.MONGODB_URL || 'mongodb://localhost:27017/tickietackietoe'
+
 app.set('view engine', 'pug')
 
-
-//middleware
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index'))
 
-mongoose.connect(MONGOD_URL, () => {
-	server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () => {
+  server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+})
+
+const Game = mongoose.model('game', {
+  board: [
+    [String, String, String],
+    [String, String, String],
+    [String, String, String],
+  ],
+  toMove: String,
+  result: String,
 })
 
 io.on('connect', socket => {
-	console.log(`socket connected: ${socket.id}`)
-	socket.on('disconnect', () => console.log('Socket Disconnected'))
+  Game.create({
+    board: [['','',''],['','',''],['','','']],
+    toMove: 'X',
+  })
+  .then(g => {
+    socket.game = g
+    socket.emit('new game', g)
+  })
+  .catch(err => {
+    socket.emit('error', err)
+    console.error(err)
+  })
+
+  console.log(`Socket connected: ${socket.id}`)
+
+  socket.on('make move', move => makeMove(move, socket))
+  socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
 })
 
+const makeMove = (move, socket) => {
+    if (isFinished(socket.game) || !isSpaceAvailable(socket.game, move)) {
+      return
+    }
+
+    Promise.resolve()
+      .then(() => setMove(socket.game, move))
+      .then(toggleNextMove)
+      .then(setResult)
+      .then(g => g.save())
+      .then(g => socket.emit('move made', g))
+}
+
+const isFinished = game => !!game.result
+const isSpaceAvailable = (game, move) => !game.board[move.row][move.col]
+const setMove = (game, move) => {
+  game.board[move.row][move.col] = game.toMove
+  game.markModified('board') // trigger mongoose change detection
+  return game
+}
+const toggleNextMove = game => {
+  game.toMove = game.toMove === 'X' ? 'O' : 'X'
+  return game
+}
+const setResult = game => {
+  const result = winner(game.board)
+
+  if (result) {
+    game.toMove = undefined // mongoose equivalent to: `delete socket.game.toMove`
+    game.result = result
+  }
+
+  return game
+}
+
+const winner = b => {
+  // Rows
+  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
+    return b[0][0]
+  }
+
+  if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
+    return b[1][0]
+  }
+
+  if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
+    return b[2][0]
+  }
+
+  // Cols
+  if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
+    return b[0][0]
+  }
+
+  if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
+    return b[0][1]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
+    return b[0][2]
+  }
+
+  // Diags
+  if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+    return b[0][0]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+    return b[0][2]
+  }
+
+  // Tie or In-Progress
+  else {
+    return null
+  }
+}
